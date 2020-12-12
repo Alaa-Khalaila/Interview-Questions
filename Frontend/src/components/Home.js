116c5b084d422007fa6750ea22f8cbbf51a4c3ee
Alaa-Khalaila <alkhalila.alaa@gmail.com>
2020-12-13 00:07:27 +0200

    Project Finsished

--- Frontend/src/components/Home.js
+++ Frontend/src/components/Home.js
@@ -4,13 +4,13 @@ import {Link} from 'react-router-dom'
 const Home = () => {
     return (
         <div className="container">
+            <h3>Welcome To<span> Interview</span>-Questions.com</h3>
             <div className="box">
                 <h1>Pick subject</h1>
                 <div className="buttons">
                     <Link to='/node'><button>Node.js</button></Link>
                     <Link to='/react'><button>React.js</button></Link>
                     <Link to='/devlopers'><button>For Devlopers</button></Link>
-                    <Link to='/all'><button>All subjects</button></Link>
                 </div>
             </div>
         </div>
